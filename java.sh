afd46103d72886b75dd3372ab87589d9394df40b
Kowndinya2000 <cs17b032@iittp.ac.in>
2020-05-23 21:24:43 +0530

    Second commit

--- java.sh
+++ java.sh
@@ -1,100 +1,100 @@
-git clone https://github.com/a-schild/jave2
-git clone https://github.com/AdamBien/javaee7-essentials-archetype
-git clone https://github.com/AdamBien/javaee8-essentials-archetype
-git clone https://github.com/apssouza22/java-microservice
-git clone https://github.com/attenzione/android-ColorPickerPreference
-git clone https://github.com/aws-samples/aws-java-sample
-git clone https://github.com/b0noI/TicTacToe
-git clone https://github.com/buddy-works/simple-java-project
-git clone https://github.com/bytedeco/sample-projects
-git clone https://github.com/cacheflowe/haxademic
-git clone https://github.com/callicoder/javafx-examples
-git clone https://github.com/cfdobber/maven-java9-jigsaw
-git clone https://github.com/citrusframework/citrus-samples
-git clone https://github.com/code-not-found/grpc-java
-git clone https://github.com/cowtowncoder/java-uuid-generator
-git clone https://github.com/devopstrainingblr/Maven-Java-Project
-git clone https://github.com/dmpe/JavaFX
-git clone https://github.com/eclipse/jetty.project
-git clone https://github.com/eclipse/paho.mqtt.java
-git clone https://github.com/edmund-wagner/junrar
-git clone https://github.com/FacePlusPlus/facepp-java-sdk
-git clone https://github.com/frandorado/spring-projects
-git clone https://github.com/FutureProcessing/spring-boot-security-example
-git clone https://github.com/galatea-associates/fuse-starter-java
-git clone https://github.com/getgauge-examples/java-gradle-selenium
-git clone https://github.com/googlearchive/tango-examples-java
-git clone https://github.com/GustavoRolimSantos/Java
-git clone https://github.com/gwhalin/Memcached-Java-Client
-git clone https://github.com/hannonhill/Webservices-Java-Sample-Project
-git clone https://github.com/heremaps/here-android-sdk-examples
-git clone https://github.com/hibernate/hibernate-demos
-git clone https://github.com/hope-for/hope-cloud
-git clone https://github.com/hprose/hprose-java
-git clone https://github.com/Instagram/ig-json-parser
-git clone https://github.com/internetarchive/heritrix3
-git clone https://github.com/janpetryk/reddit-bot
-git clone https://github.com/jasonray/jersey-starterkit
-git clone https://github.com/javaparser/javaparser-maven-sample
-git clone https://github.com/jesuino/java-ml-projects
-git clone https://github.com/joaomlneto/travis-ci-tutorial-java
-git clone https://github.com/journaldev/journaldev
-git clone https://github.com/k55k32/cms-admin-end
-git clone https://github.com/kelemen/netbeans-gradle-project
-git clone https://github.com/kevinsawicki/github-maven-example
-git clone https://github.com/labmac/java-project
-git clone https://github.com/linuxacademy/content-jenkins-java-project
-git clone https://github.com/lkuza2/java-speech-api
-git clone https://github.com/m-reza-rahman/javaee-javascript
-git clone https://github.com/mcourteaux/CraftMania
-git clone https://github.com/microsoft/azure-tools-for-java
-git clone https://github.com/mplacona/java-junit-template-project
-git clone https://github.com/MyCollab/mycollab
-git clone https://github.com/nacos-group/nacos-spring-boot-project
-git clone https://github.com/nacos-group/nacos-spring-project
-git clone https://github.com/nayuki/Project-Euler-solutions
-git clone https://github.com/openjdk/jmc
-git clone https://github.com/openjdk/loom
-git clone https://github.com/openjdk/skara
-git clone https://github.com/OpenNTF/JavascriptAggregator
-git clone https://github.com/opentracing-contrib/java-spring-cloud
-git clone https://github.com/OvercastNetwork/ProjectAres
-git clone https://github.com/OWASP/owasp-java-encoder
-git clone https://github.com/panterch/ch.panter.projectzero
-git clone https://github.com/peterarsentev/games_oop_javafx
-git clone https://github.com/phaniprasad009/Ant-JavaProject
-git clone https://github.com/pluralsight-projects/Java-BookStore
-git clone https://github.com/pluralsight-projects/Java-BookStoreShoppingCart
-git clone https://github.com/quickfix-j/quickfixj
-git clone https://github.com/Red5/red5-examples
-git clone https://github.com/sap-production/XcodeProjectJavaAPI
-git clone https://github.com/sarxos/webcam-capture
-git clone https://github.com/schildbach/public-transport-enabler
-git clone https://github.com/seis665/java-project
-git clone https://github.com/sheefee/simple-sso
-git clone https://github.com/shouryaj98/Hotel-Management-Project-Java
-git clone https://github.com/skeeto/sample-java-project
-git clone https://github.com/slgobinath/Java-Helps-Android
-git clone https://github.com/slgobinath/Java-Helps-Java
-git clone https://github.com/sormuras/bach
-git clone https://github.com/soumyadip007/Java-JavaFx-Swing-Projects-Desktop-Application-GUI-Software
-git clone https://github.com/spring-guides/gs-gradle
-git clone https://github.com/spring-guides/gs-maven
-git clone https://github.com/SquareSquash/java
-git clone https://github.com/stoicflame/enunciate
-git clone https://github.com/StubbornJava/StubbornJava
-git clone https://github.com/teamed/qulice
-git clone https://github.com/TEAMMATES/teammates
-git clone https://github.com/testdouble/java-testing-example
-git clone https://github.com/thorntail/thorntail
-git clone https://github.com/tomsquest/java-agent-asm-javassist-sample
-git clone https://github.com/treehouse-projects/java-debugging-flashy
-git clone https://github.com/treehouse-projects/java-unittesting-vendingmachine
-git clone https://github.com/TTFHW/jenkins_pipeline_java_maven
-git clone https://github.com/udacity/ud282
-git clone https://github.com/vogellacompany/codeexamples-javaweb
-git clone https://github.com/wantao666/java-project0
-git clone https://github.com/web3j/sample-project-gradle
-git clone https://github.com/Yalantis/FlipViewPager.Draco
-git clone https://github.com/yrojha4ever/JavaStud
-git clone https://github.com/yusufshakeel/Java-Image-Processing-Project
+sudo git clone https://github.com/a-schild/jave2
+sudo git clone https://github.com/AdamBien/javaee7-essentials-archetype
+sudo git clone https://github.com/AdamBien/javaee8-essentials-archetype
+sudo git clone https://github.com/apssouza22/java-microservice
+sudo git clone https://github.com/attenzione/android-ColorPickerPreference
+sudo git clone https://github.com/aws-samples/aws-java-sample
+sudo git clone https://github.com/b0noI/TicTacToe
+sudo git clone https://github.com/buddy-works/simple-java-project
+sudo git clone https://github.com/bytedeco/sample-projects
+sudo git clone https://github.com/cacheflowe/haxademic
+sudo git clone https://github.com/callicoder/javafx-examples
+sudo git clone https://github.com/cfdobber/maven-java9-jigsaw
+sudo git clone https://github.com/citrusframework/citrus-samples
+sudo git clone https://github.com/code-not-found/grpc-java
+sudo git clone https://github.com/cowtowncoder/java-uuid-generator
+sudo git clone https://github.com/devopstrainingblr/Maven-Java-Project
+sudo git clone https://github.com/dmpe/JavaFX
+sudo git clone https://github.com/eclipse/jetty.project
+sudo git clone https://github.com/eclipse/paho.mqtt.java
+sudo git clone https://github.com/edmund-wagner/junrar
+sudo git clone https://github.com/FacePlusPlus/facepp-java-sdk
+sudo git clone https://github.com/frandorado/spring-projects
+sudo git clone https://github.com/FutureProcessing/spring-boot-security-example
+sudo git clone https://github.com/galatea-associates/fuse-starter-java
+sudo git clone https://github.com/getgauge-examples/java-gradle-selenium
+sudo git clone https://github.com/googlearchive/tango-examples-java
+sudo git clone https://github.com/GustavoRolimSantos/Java
+sudo git clone https://github.com/gwhalin/Memcached-Java-Client
+sudo git clone https://github.com/hannonhill/Webservices-Java-Sample-Project
+sudo git clone https://github.com/heremaps/here-android-sdk-examples
+sudo git clone https://github.com/hibernate/hibernate-demos
+sudo git clone https://github.com/hope-for/hope-cloud
+sudo git clone https://github.com/hprose/hprose-java
+sudo git clone https://github.com/Instagram/ig-json-parser
+sudo git clone https://github.com/internetarchive/heritrix3
+sudo git clone https://github.com/janpetryk/reddit-bot
+sudo git clone https://github.com/jasonray/jersey-starterkit
+sudo git clone https://github.com/javaparser/javaparser-maven-sample
+sudo git clone https://github.com/jesuino/java-ml-projects
+sudo git clone https://github.com/joaomlneto/travis-ci-tutorial-java
+sudo git clone https://github.com/journaldev/journaldev
+sudo git clone https://github.com/k55k32/cms-admin-end
+sudo git clone https://github.com/kelemen/netbeans-gradle-project
+sudo git clone https://github.com/kevinsawicki/github-maven-example
+sudo git clone https://github.com/labmac/java-project
+sudo git clone https://github.com/linuxacademy/content-jenkins-java-project
+sudo git clone https://github.com/lkuza2/java-speech-api
+sudo git clone https://github.com/m-reza-rahman/javaee-javascript
+sudo git clone https://github.com/mcourteaux/CraftMania
+sudo git clone https://github.com/microsoft/azure-tools-for-java
+sudo git clone https://github.com/mplacona/java-junit-template-project
+sudo git clone https://github.com/MyCollab/mycollab
+sudo git clone https://github.com/nacos-group/nacos-spring-boot-project
+sudo git clone https://github.com/nacos-group/nacos-spring-project
+sudo git clone https://github.com/nayuki/Project-Euler-solutions
+sudo git clone https://github.com/openjdk/jmc
+sudo git clone https://github.com/openjdk/loom
+sudo git clone https://github.com/openjdk/skara
+sudo git clone https://github.com/OpenNTF/JavascriptAggregator
+sudo git clone https://github.com/opentracing-contrib/java-spring-cloud
+sudo git clone https://github.com/OvercastNetwork/ProjectAres
+sudo git clone https://github.com/OWASP/owasp-java-encoder
+sudo git clone https://github.com/panterch/ch.panter.projectzero
+sudo git clone https://github.com/peterarsentev/games_oop_javafx
+sudo git clone https://github.com/phaniprasad009/Ant-JavaProject
+sudo git clone https://github.com/pluralsight-projects/Java-BookStore
+sudo git clone https://github.com/pluralsight-projects/Java-BookStoreShoppingCart
+sudo git clone https://github.com/quickfix-j/quickfixj
+sudo git clone https://github.com/Red5/red5-examples
+sudo git clone https://github.com/sap-production/XcodeProjectJavaAPI
+sudo git clone https://github.com/sarxos/webcam-capture
+sudo git clone https://github.com/schildbach/public-transport-enabler
+sudo git clone https://github.com/seis665/java-project
+sudo git clone https://github.com/sheefee/simple-sso
+sudo git clone https://github.com/shouryaj98/Hotel-Management-Project-Java
+sudo git clone https://github.com/skeeto/sample-java-project
+sudo git clone https://github.com/slgobinath/Java-Helps-Android
+sudo git clone https://github.com/slgobinath/Java-Helps-Java
+sudo git clone https://github.com/sormuras/bach
+sudo git clone https://github.com/soumyadip007/Java-JavaFx-Swing-Projects-Desktop-Application-GUI-Software
+sudo git clone https://github.com/spring-guides/gs-gradle
+sudo git clone https://github.com/spring-guides/gs-maven
+sudo git clone https://github.com/SquareSquash/java
+sudo git clone https://github.com/stoicflame/enunciate
+sudo git clone https://github.com/StubbornJava/StubbornJava
+sudo git clone https://github.com/teamed/qulice
+sudo git clone https://github.com/TEAMMATES/teammates
+sudo git clone https://github.com/testdouble/java-testing-example
+sudo git clone https://github.com/thorntail/thorntail
+sudo git clone https://github.com/tomsquest/java-agent-asm-javassist-sample
+sudo git clone https://github.com/treehouse-projects/java-debugging-flashy
+sudo git clone https://github.com/treehouse-projects/java-unittesting-vendingmachine
+sudo git clone https://github.com/TTFHW/jenkins_pipeline_java_maven
+sudo git clone https://github.com/udacity/ud282
+sudo git clone https://github.com/vogellacompany/codeexamples-javaweb
+sudo git clone https://github.com/wantao666/java-project0
+sudo git clone https://github.com/web3j/sample-project-gradle
+sudo git clone https://github.com/Yalantis/FlipViewPager.Draco
+sudo git clone https://github.com/yrojha4ever/JavaStud
+sudo git clone https://github.com/yusufshakeel/Java-Image-Processing-Project
